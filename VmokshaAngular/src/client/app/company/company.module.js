bb0a8edf5e53756ba1ff4f738c1e31c0ef02d11f
Kirti3252 <kirti.irkal@vmokshagroup.com>
2017-01-18 16:46:05 +0530

    Update company.module.js

--- VmokshaAngular/src/client/app/company/company.module.js
+++ VmokshaAngular/src/client/app/company/company.module.js
@@ -1,3 +1,12 @@
+/* Description: The Company module deals with maintaining functionality related to the Company pages.
+ * Author:  Kirti
+ * Created On: 29/12/2016
+ * Modified For:
+ * Modified On: 
+ * Modified By:
+ * */
+
+
 (function() {
     'use strict';
     angular.module('app.company', ['app.core']);
