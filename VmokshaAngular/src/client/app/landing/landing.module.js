26cd0da1dc524d680a5edc18b7e615cc3a42e5f5
Kirti3252 <kirti.irkal@vmokshagroup.com>
2017-01-18 16:43:24 +0530

    Update landing.module.js

--- VmokshaAngular/src/client/app/landing/landing.module.js
+++ VmokshaAngular/src/client/app/landing/landing.module.js
@@ -1,3 +1,12 @@
+
+/* Description: The landing module deals with maintaining functionality related to the landing page.
+ * Author:  Kirti
+ * Created On: 29/12/2016
+ * Modified For: 
+ * Modified On: 
+ * Modified By:
+ * */
+
 (function() {
     'use strict';
     angular.module('app.landing', ['app.core']);
