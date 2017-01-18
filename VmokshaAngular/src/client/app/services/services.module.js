fd99bbae47b896f50948cf7898845d59451b2bbf
Kirti3252 <kirti.irkal@vmokshagroup.com>
2017-01-18 16:40:15 +0530

    Updated  services Module
    by kirti

---
+++ VmokshaAngular/src/client/app/services/services.module.js
@@ -0,0 +1,13 @@
+
+/* Description: The services module deals with maintaining functionality related to the services pages.
+ * Author:  Kirti
+ * Created On: 16-01-2017
+ * Modified For: 
+ * Modified On: 
+ * Modified By:
+ * */
+
+(function() {
+    'use strict';
+    angular.module('app.services', ['app.core']);
+})();
